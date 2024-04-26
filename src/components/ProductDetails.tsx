0d91964087c589a085390c0c51629f2bf4ed4229
damayamdev <164294520+damayamdev@users.noreply.github.com>
2024-04-26 14:23:02 -0500

    editar y unificación del formulario

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import {useNavigate} from 'react-router-dom'
 import type { Product } from "../types";
 import { formatCurrency } from "../utils";
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 
 type ProductDetailsProps = {
   product: Product;
@@ -22,8 +23,11 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
       </td>
       <td className="p-3 text-justify text-lg text-gray-800 ">
         <div className="flex gap-2 items-center">
-          <button onClick={() => navigate(`productos/${product.id}/editar`)} className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center">
-            Editar
+          <button onClick={() => navigate(`productos/${product.id}/editar`)} className="bg-indigo-600 text-white rounded-lg p-2 uppercase font-bold text-xs text-center">
+            <PencilIcon className='w-5'/>
+          </button>
+          <button onClick={() => navigate(`productos/${product.id}/editar`)} className="bg-red-600 text-white rounded-lg p-2 uppercase font-bold text-xs text-center">
+            <TrashIcon className='w-5'/>
           </button>
         </div>
       </td>
--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -10,6 +10,7 @@ import {
 import ErrorMessage from "../components/ErrorMessage";
 import { updateProduct, getProductById } from "../services/ProductService";
 import { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   if (params.id !== undefined) {
@@ -62,32 +63,7 @@ const EditProduct = () => {
       {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <Form method="post" className="mt-10">
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-            defaultValue={product.name}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-            defaultValue={product.price}
-          />
-        </div>
+        <ProductForm product={product} />
         <div className="mb-4">
           <label className="text-gray-800" htmlFor="availability">
             Disponibilidad:
